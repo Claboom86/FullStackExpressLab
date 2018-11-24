c82506a13ab1912dc06b314d1d84ce13ccbf4e33
Claboom86 <betsybass@gmail.com>
2018-11-24 17:13:43 -0500

    More styling

--- public/app/components/list/cart.js
+++ public/app/components/list/cart.js
@@ -1,21 +1,28 @@
 "use strict";
 const cart = {
     template: `
+<section id="add-container">
+    <form ng-submit="$ctrl.addItem($ctrl.newItem);">
+        <input class="new-item" type="text" ng-model="$ctrl.newItem.product" placeholder="Product Name">
+        <input class="new-item" type="text" ng-model="$ctrl.newItem.price" placeholder="Price">
+        <input class="new-item" type="text" ng-model="$ctrl.newItem.quantity" placeholder="Quantity">
+        <button id="add-btn">Add Item</button>
+    </form>
+</section>
 
-<form ng-submit="$ctrl.addItem($ctrl.newItem);">
-    <input type="text" ng-model="$ctrl.newItem.product" placeholder="product name">
-    <input type="text" ng-model="$ctrl.newItem.price" placeholder="price">
-    <input type="text" ng-model="$ctrl.newItem.quantity" placeholder="quantity">
-    <button>Add Item</button>
-</form>
+<h2>Cart</h2>
 
 <section class="main-container">
     <section class="cart-container" ng-repeat="obj in $ctrl.items">
-        <input class="update__input" type="text" ng-blur="$ctrl.updateItem(obj);" ng-model="obj.product">
-        <input class="update__input" type="text" ng-blur="$ctrl.updateItem(obj);" ng-model="obj.price">
-        <input class="update__input" type="text" ng-blur="$ctrl.updateItem(obj);" ng-model="obj.quantity">
+        <input id="product" type="text" ng-blur="$ctrl.updateItem(obj);" ng-model="obj.product">
+        <section id="price-container">
+            <label>Price: $</label>
+            <input id="price" type="text" ng-blur="$ctrl.updateItem(obj);" ng-model="obj.price">
+         </section>
+            <label>Quantity: </label>
+        <input id="quantity" class="update-input" type="text" ng-blur="$ctrl.updateItem(obj);" ng-model="obj.quantity">
 
-        <button ng-click="$ctrl.removeItem(obj.id);">X</button>
+        <button id="delete-btn" ng-click="$ctrl.removeItem(obj.id);">X</button>
 
     </section>
 </section>
